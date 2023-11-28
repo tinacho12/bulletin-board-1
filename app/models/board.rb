df92f13c4982a1cbef4ffc8328250bec0e774dab
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-11-28 00:22:32 +0000

    Updated Boards#Show

--- app/models/board.rb
+++ app/models/board.rb
@@ -8,4 +8,10 @@
 #  updated_at :datetime         not null
 #
 class Board < ApplicationRecord
+  # Scoped direct association
+  # Board#ActivePosts: returns rows from the posts table associated with this board by board_id column, where the expires_on date is before today
+  has_many(:active_posts, -> { where("expires_on < ?", Date.today) }, class_name: "Post", foreign_key: :board_id)
+  # Board#ExpiredPosts: returns rows from the posts table associated with this board by board_id column, where the expires_on date is after today
+  has_many(:active_posts, -> { where("expires_on > ?", Date.today) }, class_name: "Post", foreign_key: :board_id)
+
 end
